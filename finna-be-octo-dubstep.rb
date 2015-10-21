bbe19d94622e6d704472d8e1d93cee4722f6dc24
octosquidport <Octosquid@pal-nat184-168-020.itap.purdue.edu>
2015-10-21 12:23:20 -0400

    hopefully this time

--- finna-be-octo-dubstep.rb
+++ finna-be-octo-dubstep.rb
@@ -2,9 +2,9 @@ class FinnaBeOctoDubstep < Formula
 	homepage "https://github.com/octosquidport/finna-be-octo-dubstep"
 	url "https://github.com/octosquidport/finna-be-octo-dubstep/archive/1.0.0.tar.gz"
 	version "1.0.0"
-	sha256 "8fd57c300218ff676582651dca19131abaa36a92d317d6db7843b342786c55d0"
+	sha256 "e728979da61d78f00f61a9c009185e613f00f62e383211459007546987bcb480"
 
 	def install
-		bin.install Dir["praiseprose"]
+		bin.install "praiseprose"
 	end
 end
