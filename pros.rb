cf7f46d5088fa880d1c20059813f38186eb2279d
octosquidport <Octosquid@pal-nat184-171-085.itap.purdue.edu>
2015-11-11 22:08:06 -0500

    fixed script

--- pros.rb
+++ pros.rb
@@ -2,7 +2,8 @@ class FinnaBeOctoDubstep < Formula
 	homepage "https://github.com/octosquidport/PROS-mac-osx"
 	url "https://github.com/octosquidport/PROS-mac-osx/archive/1.0.0.tar.gz"
 	version "1.0.0"
-
+	keg-only "To preserve symbols for testing"
+	
 	def install
 		bin.install "Eclipse.app"
 	end
