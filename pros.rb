7fafbe4316fd6b2b3d926c32770abe9660b142d1
octosquidport <Octosquid@pal-nat184-171-085.itap.purdue.edu>
2015-11-11 22:01:22 -0500

    added pros

---
+++ pros.rb
@@ -0,0 +1,9 @@
+class FinnaBeOctoDubstep < Formula
+	homepage "https://github.com/octosquidport/finna-be-octo-dubstep"
+	url "https://github.com/octosquidport/PROS-mac-osx/archive/1.0.0.tar.gz"
+	version "1.0.0"
+
+	def install
+		bin.install "Eclipse.app"
+	end
+end
